e3c34cc826e9f818aeb5369eba3c9aa1e415d5dc
raoulperselloSMM <software@raoulpersello.com>
2026-01-31 23:24:00 +0100

    Initialize Firestore and Auth in Firebase
    Added Firestore and Auth initialization to Firebase setup.

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAdQ6KtrIHleAHiqjAfxFKCXPMT-oUoCjE",
   authDomain: "farmasocial-266de.firebaseapp.com",
@@ -13,5 +11,6 @@ const firebaseConfig = {
   appId: "1:172116157552:web:e6a657053fdb2f1f1040b5"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
