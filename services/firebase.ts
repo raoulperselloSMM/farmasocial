9eba52bce7ab316fc13bda67499de6a6f621f7b5
raoulperselloSMM <software@raoulpersello.com>
2026-01-31 23:00:22 +0100

    Initialize Firebase with configuration
    Set up Firebase configuration and initialization.

---
+++ services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdQ6KtrIHleAHiqjAfxFKCXPMT-oUoCjE",
+  authDomain: "farmasocial-266de.firebaseapp.com",
+  projectId: "farmasocial-266de",
+  storageBucket: "farmasocial-266de.firebasestorage.app",
+  messagingSenderId: "172116157552",
+  appId: "1:172116157552:web:e6a657053fdb2f1f1040b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
